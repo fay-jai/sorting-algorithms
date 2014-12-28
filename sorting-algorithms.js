772d2e17df35535a7515f38fec84912ce3ae7d21
Willson Mock <willson.mock@gmail.com>
2014-12-28 12:12:19 -0500

    Add sorting-algorithms.js - only shell functions for now

---
+++ sorting-algorithms.js
@@ -0,0 +1,25 @@
+(function (global) {
+  var _sa = {};
+
+  _sa.selectionSort = function (array) {
+
+  };
+
+  _sa.bubbleSort = function (array) {
+
+  };
+
+  _sa.insertionSort = function (array) {
+
+  };
+
+  _sa.mergeSort = function (array) {
+
+  };
+
+  _sa.quickSort = function (array) {
+
+  };
+
+  global._sa = _sa;
+})(window);
