1ba24d1733573daed4a13001965b9398e519f933
Willson Mock <willson.mock@gmail.com>
2015-02-10 17:26:55 -0800

    Add shell for sorting algorithms in ruby

---
+++ ruby/sorting-algorithms.rb
@@ -0,0 +1,30 @@
+module Sort_Algorithms
+  # selection sort
+  def self.selection_sort(array)
+
+  end
+
+  # bubble sort
+  def self.bubble_sort(array)
+
+  end
+
+  # insertion sort
+  def self.insertion_sort(array)
+
+  end
+
+  # merge sort
+  def self.merge_sort(array)
+
+  end
+
+  # quick sort
+  def self.quick_sort(array)
+
+  end
+
+  def self.swap(array, i, j)
+
+  end
+end
