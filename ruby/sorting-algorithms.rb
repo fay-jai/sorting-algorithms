9965a02ef11c77260a8cd2a048a7342fcbbc5ab0
Willson Mock <willson.mock@gmail.com>
2015-02-11 15:13:23 -0800

    Implement bubble sort and insertion sort in ruby

--- ruby/sorting-algorithms.rb
+++ ruby/sorting-algorithms.rb
@@ -1,29 +1,51 @@
 module Sort_Algorithms
+  # Example arrays
+  # @empty   = []
+  # @one     = [1]
+  # @inorder = [-5, -1, 0, 4, 9]
+  # @reverse = [10, 8, 5, 3, -1, -7]
+  # @random  = [-1, 6, 10, 2, -6, -1, 0, -3, 8, 9, 6, 10]
+
   # selection sort
   def self.selection_sort(array)
     len = array.length
-
     for i in 0...len
       min_idx = i
-
       for j in (i + 1)...len
         min_idx = j if array[min_idx] > array[j]
       end
-
       swap(array, min_idx, i)
     end
-
-    return array
+    array
   end
 
   # bubble sort
   def self.bubble_sort(array)
-
+    len = array.length
+    for i in 0...len
+      has_swapped = false
+      for j in 0...(len - i - 1)
+        swap(array, j, j + 1) if array[j] > array[j + 1]
+        has_swapped = true
+      end
+      break if not has_swapped
+    end
+    array
   end
 
   # insertion sort
   def self.insertion_sort(array)
-
+    len = array.length
+    for i in 0...len
+      hole = array[i]
+      j = i
+      while j > 0 && array[j - 1] > hole
+        array[j] = array[j - 1]
+        j -= 1
+      end
+      array[j] = hole
+    end
+    array
   end
 
   # merge sort
