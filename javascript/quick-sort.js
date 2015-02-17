5ab8b9fb17f379e0ae6be75739475f167baebfd8
Willson Mock <willson.mock@gmail.com>
2015-02-17 14:36:33 -0800

    Complete quick-sort.js

--- javascript/quick-sort.js
+++ javascript/quick-sort.js
@@ -5,5 +5,15 @@ var reverse = [10, 8, 5, 3, -1, -7];
 var random  = [-1, 6, 10, 2, -6, -1, 0, -3, 8, 9, 6, 10];
 
 var quickSort = function (array) {
+  if (array.length < 2) return array;
 
+  var pivot = array.splice(0, 1);
+  var left  = [];
+  var right = [];
+
+  array.forEach(function (value) {
+    (value <= pivot[0] ? left : right).push(value);
+  });
+
+  return Array.prototype.concat(quickSort(left), pivot, quickSort(right));
 };
