e88c85db425b35e106d865abeaec76c4b1e60ec4
Willson Mock <willson.mock@gmail.com>
2015-02-17 14:31:54 -0800

    Complete merge-sort.js in javascript

--- javascript/merge-sort.js
+++ javascript/merge-sort.js
@@ -5,9 +5,34 @@ var reverse = [10, 8, 5, 3, -1, -7];
 var random  = [-1, 6, 10, 2, -6, -1, 0, -3, 8, 9, 6, 10];
 
 var mergeSort = function (array) {
+  var len = array.length;
+  var mid, left, right;
 
+  // base case
+  if (len < 2) return array;
+
+  // recursive case
+  mid   = Math.floor(len / 2);
+  left  = mergeSort( array.slice(0, mid) );
+  right = mergeSort( array.slice(mid) );
+
+  return merge(left, right);
 };
 
-var merge = function (array, left, right) {
+var merge = function (left, right) {
+  var result   = [];
 
+  while (left.length > 0 && right.length > 0) {
+    result.push( left[0] <= right[0] ? left.shift() : right.shift() );
+  }
+
+  while (left.length > 0) {
+    result.push( left.shift() );
+  }
+
+  while (right.length > 0) {
+    result.push( right.shift() );
+  }
+
+  return result;
 };
