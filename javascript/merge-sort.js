ffc0bc02156adc75d71e3551af86d6ba4a098d9b
Willson Mock <willson.mock@gmail.com>
2015-02-17 13:07:46 -0800

    Add shell for sorting algorithms in javascript

---
+++ javascript/merge-sort.js
@@ -0,0 +1,13 @@
+var empty   = [];
+var one     = [1];
+var inorder = [-5, -1, 0, 4, 9];
+var reverse = [10, 8, 5, 3, -1, -7];
+var random  = [-1, 6, 10, 2, -6, -1, 0, -3, 8, 9, 6, 10];
+
+var mergeSort = function (array) {
+
+};
+
+var merge = function (array, left, right) {
+
+};
